e03534fb8c8a40751f6070d4094c4e29c129ce1f
jeongmin-dev <ljeongmin0908@gmail.com>
2022-03-11 01:59:33 +0900

    feat: 아티클 상세 페이지 작업중

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -7,7 +7,267 @@ import Label from "../elements/Label";
 import Title from "../elements/Title";
 
 const ArticleCard = props => {
+  const { isRead, done, isMe, isDetail } = props;
+
   const navigate = useNavigate();
+
+  if (isRead) {
+    return (
+      <React.Fragment>
+        <ImageArticle
+          onClick={() => {
+            navigate("/article");
+          }}
+          isRead={isRead}
+          done={done}
+        >
+          <ReadMark />
+          <div style={{ position: "absolute", top: "28px", right: "28px" }}>
+            <img
+              src="/images/bookmarkOn.png"
+              alt="bookmark"
+              width="24px"
+              height="24px"
+            />
+          </div>
+          <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
+            <div style={{ display: "flex", paddingBottom: "8px" }}>
+              <Label text="커리어" />
+              <Label text="디자인" />
+            </div>
+            <Title
+              _titleSize={({ theme }) => theme.fontSizes.font18}
+              _subtitleSize={({ theme }) => theme.fontSizes.font14}
+              lineHeight="24px"
+            >
+              <h1 style={{ color: "#ffffff" }}>
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다
+                기록을
+              </h1>
+              <p
+                style={{
+                  color: "#ffffff",
+                  opacity: 0.6,
+                  lineHeight: "18px",
+                  paddingTop: "8px",
+                }}
+              >
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기...
+              </p>
+            </Title>
+          </div>
+        </ImageArticle>
+        {/* 사진이 없는 아티클 경우 - 읽은 후 */}
+        <DefaultArticle
+          onClick={() => {
+            navigate("/article");
+          }}
+          isRead={isRead}
+          done={done}
+        >
+          <ReadMark />
+          <div style={{ position: "absolute", top: "28px", right: "28px" }}>
+            <img
+              src="/images/bookmarkOff.png"
+              alt="bookmark"
+              width="24px"
+              height="24px"
+            />
+          </div>
+
+          <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
+            <div style={{ display: "flex", paddingBottom: "8px" }}>
+              <Label text="커리어" />
+              <Label text="디자인" />
+            </div>
+            <Title
+              _titleSize={({ theme }) => theme.fontSizes.font18}
+              _subtitleSize={({ theme }) => theme.fontSizes.font14}
+              lineHeight="24px"
+            >
+              <h1 style={{ color: "#ffffff" }}>
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다
+                기록을
+              </h1>
+              <p
+                style={{
+                  color: "#ffffff",
+                  opacity: 0.6,
+                  lineHeight: "18px",
+                  paddingTop: "8px",
+                }}
+              >
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기...
+              </p>
+            </Title>
+          </div>
+        </DefaultArticle>
+      </React.Fragment>
+    );
+  }
+
+  if (isMe) {
+    return (
+      <React.Fragment>
+        {/* 사진이 없는 아티클 */}
+        <DefaultArticle
+          onClick={() => {
+            navigate("/article");
+          }}
+          isMe={isMe}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <DesignObj />
+            <Label _label="big" text="안 읽은지 7일째.." />
+          </div>
+
+          <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
+            <div style={{ display: "flex", paddingBottom: "8px" }}>
+              <Label text="커리어" />
+              <Label text="디자인" />
+            </div>
+            <Title
+              _titleSize={({ theme }) => theme.fontSizes.font18}
+              _subtitleSize={({ theme }) => theme.fontSizes.font14}
+              lineHeight="24px"
+            >
+              <h1 style={{ color: "#ffffff" }}>
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다
+                기록을
+              </h1>
+              <p
+                style={{
+                  color: "#ffffff",
+                  opacity: 0.6,
+                  lineHeight: "18px",
+                  paddingTop: "8px",
+                }}
+              >
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기...
+              </p>
+            </Title>
+          </div>
+        </DefaultArticle>
+        {/* 사진이 있는 아티클 */}
+        <ImageArticle
+          onClick={() => {
+            navigate("/article");
+          }}
+          isMe={isMe}
+          done={done}
+        >
+          <div
+            style={{
+              width: "22px",
+              height: "22px",
+              backgroundColor: "#353C49",
+              position: "absolute",
+              right: "0px",
+              bottom: "0px",
+              borderRadius: "11px",
+            }}
+          ></div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <DesignObj />
+            <Label _label="big" text="안 읽은지 7일째.." />
+          </div>
+
+          <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
+            <div style={{ display: "flex", paddingBottom: "8px" }}>
+              <Label text="커리어" />
+              <Label text="디자인" />
+            </div>
+            <Title
+              _titleSize={({ theme }) => theme.fontSizes.font18}
+              _subtitleSize={({ theme }) => theme.fontSizes.font14}
+              lineHeight="24px"
+            >
+              <h1 style={{ color: "#ffffff" }}>
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다
+                기록을
+              </h1>
+              <p
+                style={{
+                  color: "#ffffff",
+                  opacity: 0.6,
+                  lineHeight: "18px",
+                  paddingTop: "8px",
+                }}
+              >
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기...
+              </p>
+            </Title>
+          </div>
+        </ImageArticle>
+      </React.Fragment>
+    );
+  }
+
+  if (isDetail) {
+    return (
+      <React.Fragment>
+        <ImageArticle
+          onClick={() => {
+            navigate("/article");
+          }}
+          isDetail={isDetail}
+          done={done}
+        >
+          <ReadMark />
+          <div style={{ position: "absolute", top: "28px", right: "28px" }}>
+            <img
+              src="/images/link.png"
+              alt="bookmark"
+              width="24px"
+              height="24px"
+            />
+          </div>
+          <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
+            <div
+              style={{
+                display: "flex",
+                paddingBottom: "8px",
+                color: "#ffffff",
+              }}
+            >
+              <Label text="커리어" />
+              <Label text="디자인" />
+            </div>
+            <Title
+              _titleSize={({ theme }) => theme.fontSizes.font18}
+              _subtitleSize={({ theme }) => theme.fontSizes.font14}
+              lineHeight="24px"
+            >
+              <h1 style={{ color: "#ffffff" }}>
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다
+                기록을
+              </h1>
+              <p
+                style={{
+                  color: "#ffffff",
+                  opacity: 0.6,
+                  lineHeight: "18px",
+                  paddingTop: "8px",
+                }}
+              >
+                Redux Toolkit을 사용하여 간단하게 상태 관리하기...
+              </p>
+            </Title>
+          </div>
+        </ImageArticle>
+      </React.Fragment>
+    );
+  }
 
   return (
     <React.Fragment>
@@ -52,99 +312,6 @@ const ArticleCard = props => {
           </Title>
         </div>
       </DefaultArticle>
-      {/* 사진이 있는 아티클 경우 - 읽은 후 */}
-      <ImageArticle
-        onClick={() => {
-          navigate("/article");
-        }}
-      >
-        <div
-          style={{
-            width: "22px",
-            height: "22px",
-            backgroundColor: "#353C49",
-            position: "absolute",
-            right: "0px",
-            bottom: "0px",
-            borderRadius: "11px",
-          }}
-        ></div>
-        <div style={{ position: "absolute", top: "28px", right: "28px" }}>
-          <img
-            src="/images/bookmarkOff.png"
-            alt="bookmark"
-            width="24px"
-            height="24px"
-          />
-        </div>
-        <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
-          <div style={{ display: "flex", paddingBottom: "8px" }}>
-            <div>커리어</div>
-            <div>디자인</div>
-          </div>
-          <Title
-            _titleSize={({ theme }) => theme.fontSizes.font18}
-            _subtitleSize={({ theme }) => theme.fontSizes.font14}
-            lineHeight="24px"
-          >
-            <h1 style={{ color: "#ffffff" }}>
-              Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다 기록을
-            </h1>
-            <p
-              style={{
-                paddingTop: "8px",
-                color: "#ffffff",
-                opacity: 0.6,
-                lineHeight: "18px",
-              }}
-            >
-              Redux Toolkit을 사용하여 간단하게 상태 관리하기...
-            </p>
-          </Title>
-        </div>
-      </ImageArticle>
-      {/* 사진이 없는 아티클 경우 - 내 아티클 리스트 */}
-      <DefaultArticle
-        onClick={() => {
-          navigate("/article");
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <DesignObj />
-          <Label _label="big" text="안 읽은지 7일째.." />
-        </div>
-
-        <div style={{ width: "270px", position: "absolute", bottom: "28px" }}>
-          <div style={{ display: "flex", paddingBottom: "8px" }}>
-            <Label text="커리어" />
-            <Label text="디자인" />
-          </div>
-          <Title
-            _titleSize={({ theme }) => theme.fontSizes.font18}
-            _subtitleSize={({ theme }) => theme.fontSizes.font14}
-            lineHeight="24px"
-          >
-            <h1 style={{ color: "#ffffff" }}>
-              Redux Toolkit을 사용하여 간단하게 상태 관리하기 | 기억보다 기록을
-            </h1>
-            <p
-              style={{
-                color: "#ffffff",
-                opacity: 0.6,
-                lineHeight: "18px",
-                paddingTop: "8px",
-              }}
-            >
-              Redux Toolkit을 사용하여 간단하게 상태 관리하기...
-            </p>
-          </Title>
-        </div>
-      </DefaultArticle>
     </React.Fragment>
   );
 };
@@ -153,7 +320,7 @@ const DefaultArticle = styled.div`
   width: 100%;
   height: 240px;
   background-color: #505866;
-  border-radius: 20px; //isread? "20 20 80 20" : "20"
+  border-radius: ${props => (props.done ? "20px 20px 80px 20px" : "20px")};
   padding: 28px;
   margin-bottom: 16px;
   position: relative;
@@ -162,7 +329,8 @@ const DefaultArticle = styled.div`
 const ImageArticle = styled.div`
   width: 100%;
   height: 288px;
-  border-radius: 20px 20px 80px 20px;
+  border-radius: ${props =>
+    props.done === true ? "20px 20px 80px 20px" : "20px"};
   padding: 28px;
   margin-bottom: 16px;
   position: relative;
@@ -174,10 +342,22 @@ const ImageArticle = styled.div`
   background-blend-mode: darken;
 `;
 
+const ReadMark = styled.div`
+  width: 22px;
+  height: 22px;
+  background-color: ${({ theme }) => theme.colors.grayColor07};
+  position: absolute;
+  right: 0px;
+  bottom: 0px;
+  border-radius: 50%;
+`;
+
 ArticleCard.defaultProps = {
   height: "240px",
   isRead: false,
-  isImage: false,
+  isMe: false,
+  isDetail: false,
+  done: false,
 };
 
 export default ArticleCard;
--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -7,57 +7,57 @@ import Button from "../elements/Button";
 const ArticleList = props => {
   return (
     <React.Fragment>
-      <div>
-        <AlHeader>
-          <Title
-            _titleSize={({ theme }) => theme.fontSizes.font20}
-            lineHeight="24px"
-            _subtitleSize={({ theme }) => theme.fontSizes.font14}
-          >
-            <h1>아티클 폴더 이름</h1>
-            <p style={{ paddingTop: "14px" }}>24명이 도움을 받았어요</p>
+      <AlHeader>
+        <Title
+          _titleSize={({ theme }) => theme.fontSizes.font20}
+          lineHeight="24px"
+          _subtitleSize={({ theme }) => theme.fontSizes.font14}
+        >
+          <h1>아티클 폴더 이름</h1>
+          <p style={{ paddingTop: "14px" }}>24명이 도움을 받았어요</p>
 
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                padding: "8px 16px",
-                border: "0.8px solid #D2D6DA",
-                borderRadius: "8px",
-              }}
-            >
-              <img
-                src="/images/recommend.png"
-                alt="menu"
-                width="19px"
-                height="19px"
-              />
-              <p style={{ padding: 0, color: "#383838" }}>유용해요 24</p>
-            </div>
-          </Title>
-          <img src="/images/menu.png" alt="menu" width="24px" height="24px" />
-        </AlHeader>
-        <AlBox>
-          <ArticleCard />
-        </AlBox>
-        <AlButton>
-          <Title _subtitleSize="14px">
-            <p>아티클을 한번에 저장하고 싶다면?</p>
-          </Title>
-          <Button
-            isBorder
-            name="모두 저장하기"
-            bgColor="#ffffff"
-            _color="#383838"
-            _fontSize="14px"
-            height="45px"
-          />
-        </AlButton>
-      </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              padding: "8px 16px",
+              border: "0.8px solid #D2D6DA",
+              borderRadius: "8px",
+            }}
+          >
+            <img
+              src="/images/recommend.png"
+              alt="menu"
+              width="19px"
+              height="19px"
+            />
+            <p style={{ padding: 0, color: "#383838" }}>유용해요 24</p>
+          </div>
+        </Title>
+        <img src="/images/menu.png" alt="menu" width="24px" height="24px" />
+      </AlHeader>
+      <AlBox>
+        <ArticleCard />
+        <ArticleCard isRead done />
+        <ArticleCard isMe done />
+        <ArticleCard isDetail done />
+      </AlBox>
+      <AlButton>
+        <Title _subtitleSize="14px">
+          <p>아티클을 한번에 저장하고 싶다면?</p>
+        </Title>
+        <Button
+          isBorder
+          name="모두 저장하기"
+          bgColor="#ffffff"
+          _color="#383838"
+          _fontSize="14px"
+          height="45px"
+        />
+      </AlButton>
     </React.Fragment>
   );
 };
-
 const AlHeader = styled.div`
   display: flex;
   justify-content: space-between;
