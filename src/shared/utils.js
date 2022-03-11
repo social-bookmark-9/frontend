efe46cb5263ad0901be8a413d416b6eca4436b44
jeongmin-dev <ljeongmin0908@gmail.com>
2022-03-12 01:47:42 +0900

    feat: 로그인 연결 완료

--- src/shared/utils.js
+++
@@ -1,8 +0,0 @@
-export const logger = msg => {
-    if (process.env.NODE_ENV === 'production') {
-      return;
-    }
-    console.log(msg);
-    //현재 모드 확인법
-    // console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
-  };
