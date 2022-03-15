a13bea3145ebd8c21a1ea7b2b53f713f726cc8f1
jeongmin-dev <ljeongmin0908@gmail.com>
2022-03-15 23:13:26 +0900

    chore: 디자인 확인 테스트 배포

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,41 +9,53 @@ const Main = props => {
   return (
     <React.Fragment>
       <Navbar isLogin={props.isLogin} />
+      <div style={{ paddingBottom: "8px" }}></div>
       <Button
         _onClick={() => {
           navigate("/login");
         }}
+        _padding="8px"
       >
         로그인
       </Button>
+      <div style={{ paddingBottom: "8px" }}></div>
       <Button
         _onClick={() => {
           navigate("/user/nickname");
         }}
+        _padding="8px"
       >
         회원 정보
       </Button>
+      <div style={{ paddingBottom: "8px" }}></div>
       <Button
         _onClick={() => {
           navigate("/mypage");
         }}
+        _padding="8px"
       >
         마이페이지
       </Button>
+      <div style={{ paddingBottom: "8px" }}></div>
+
       <Button
         _onClick={() => {
           navigate("/articles");
         }}
+        _padding="8px"
       >
         폴더리스트 페이지
       </Button>
+      <div style={{ paddingBottom: "8px" }}></div>
       <Button
         _onClick={() => {
           navigate("/article");
         }}
+        _padding="8px"
       >
         상세페이지
       </Button>
+      <div style={{ paddingBottom: "8px" }}></div>
 
       {/* <Button
         _onClick={() => {
