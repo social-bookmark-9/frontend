3bbf7fd59efea211b442c51ef79186901aff3978
jeongmin-dev <ljeongmin0908@gmail.com>
2022-03-25 19:05:24 +0900

    setup: 중간점검 시연

--- src/redux/modules/Profile.js
+++ src/redux/modules/Profile.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import profileApi from "../app/profileApi";
-import { setFolder } from "./Folder";
 
 const ProfileApi = new profileApi();
 
