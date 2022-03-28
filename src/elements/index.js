dfc92dbcf5852ef412bf67c16427f4c61d433e2e
jeongmin-dev <72373005+jeongmin-dev@users.noreply.github.com>
2022-03-29 03:26:58 +0900

    Delete src directory

--- src/elements/index.js
+++
@@ -1,7 +0,0 @@
-import Label from "../elements/Label";
-import Image from "../elements/Image";
-import Title from "../elements/Title";
-import Text from "../elements/Text";
-import Button from "./Button";
-
-export { Label, Image, Title, Text, Button };
