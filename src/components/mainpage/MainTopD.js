09ab817942bebcdcee62ca10717c78c373462815
yonslee <yons3lee@gmail.com>
2022-03-29 20:09:48 +0900

    refactor: 불필요한 부분 수정 및 로그인 연결

--- src/components/mainpage/MainTopD.js
+++ src/components/mainpage/MainTopD.js
@@ -1,14 +1,46 @@
+// import { useRef, useEffect } from "react";
+// import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+
 import { Button, Image, Text, Title } from "../../elements";
 
+// import styled from "styled-components";
+// import lottie from "lottie-web";
+
+
 const MainTopD = () => {
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+
+//   const faceLottie = useRef();
+//   const bubbleLottie = useRef();
+
+//   useEffect(() => {
+//     lottie.loadAnimation({
+//       container: faceLottie.current,
+//       renderer: "svg",
+//       loop: true,
+//       autoplay: true,
+//       animationData: require("../../faceAnimation.json"),
+//     });
+
+//     lottie.loadAnimation({
+//       container: bubbleLottie.current,
+//       renderer: "svg",
+//       loop: true,
+//       autoplay: true,
+//       animationData: require("../../bookAnimation.json"),
+//     });
+//   }, [dispatch]);
+
   return (
-    <div style={{ backgroundColor: "#f2f3f4" }}>
+    <div style={{ backgroundColor: "#fafbfb" }}>
       <div
         style={{
           margin: "0 auto 0 auto",
           display: "flex",
           width: "1220px",
-          padding: "315px 0 336px 0",
+          padding: "315px 0 300px 0",
         }}
       >
         <div
@@ -19,6 +51,15 @@ const MainTopD = () => {
           }}
         >
           <Title _fontSize="47px" _padding="0 0 40px 0">
+            {/* <div>
+              Express yourself
+              <LottieBox style={{ verticalAlign: "middle" }} ref={faceLottie} />
+            </div>
+            <div>
+              with what you
+              <LottieBox style={{ verticalAlign: "middle" }} ref={bubbleLottie} />
+              read.
+            </div> */}
             <div style={{ marginBottom: "-10px" }}>
               Express yourself with
               <span style={{ verticalAlign: "middle" }}>
@@ -33,7 +74,7 @@ const MainTopD = () => {
               read.
             </div>
           </Title>
-          <Text _color="#505866" _fontSize="26px" _padding="0 0 24px 0">
+          <Text _color="#505866" _fontSize="26px" _padding="0 0 52px 0">
             내가 읽은 것들로 나를 표현하는 공간, 버블드
           </Text>
           <Button
@@ -44,6 +85,9 @@ const MainTopD = () => {
             _color="#383838"
             isBorder="true"
             borderRadius="5px"
+            _onClick={() => {
+              navigate("/login");
+            }}
           >
             내 버블드 만들기
           </Button>
