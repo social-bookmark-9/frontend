5eab7b7d7a57f07557e7ed9b8755afe1e4bd9cc2
jeongmin-dev <ljeongmin0908@gmail.com>
2022-04-04 23:19:07 +0900

    feat: 무한스크롤 구현

--- src/components/common/InfinityScroll.js
+++ src/components/common/InfinityScroll.js
@@ -1,44 +1,27 @@
-import React, { useCallback, useEffect } from "react";
-import _ from "lodash";
+import React, { useEffect, useRef } from "react";
 import { Spinner } from "../elements";
 
 const InfinityScroll = props => {
-  const { children, callNext, is_next, loading } = props;
-
-  const _handleScroll = _.throttle(() => {
-    if (loading) return;
-    const { innerHeight } = window;
-    const { scrollHeight } = document.body;
-
-    const scrollTop =
-      (document.documentElement && document.documentElement.scrollTop) ||
-      document.body.scrollTop;
-
-    if (scrollHeight - innerHeight - scrollTop < 200) {
-      if (loading) {
-        return;
-      }
-      callNext();
-    }
-  }, 300);
-
-  const handleScroll = useCallback(_handleScroll, [loading, _handleScroll]);
+  const { children, callNext, isNext, loading } = props;
+  const targetRef = useRef();
 
   useEffect(() => {
-    if (loading) return;
-    if (is_next) {
-      window.addEventListener("scroll", handleScroll);
-    } else {
-      window.removeEventListener("scroll", handleScroll);
-    }
+    if (loading || !isNext) return;
+    const observer = new IntersectionObserver(async entries => {
+      if (entries[0].isIntersecting) {
+        callNext();
+      }
+    });
+    const target = targetRef.current;
+    observer.observe(target);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [is_next, loading, handleScroll]);
+    return () => observer.unobserve(target);
+  }, [loading, callNext, isNext]);
 
   return (
     <>
       {children}
-      {is_next && <Spinner />}
+      <div ref={targetRef}>{isNext && <Spinner />}</div>
     </>
   );
 };
@@ -46,7 +29,7 @@ const InfinityScroll = props => {
 InfinityScroll.defaultProps = {
   children: null,
   callNext: () => {},
-  is_next: false,
+  isNext: false,
   loading: false,
 };
 
